01fa2201774a88791e80a296a0cde82a07a089e5
Shivam <shuklashivamk2@gmail.com>
2019-09-22 16:59:42 +0530

    Added listview and cursoradapter

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -28,6 +28,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.android.pets.data.PetContract.PetEntry;
@@ -53,12 +54,19 @@ public class CatalogActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
                 startActivity(intent);
+
             }
         });
 
         // To access our database, we instantiate our subclass of SQLiteOpenHelper
         // and pass the context, which is the current activity.
         mDbHelper = new PetDbHelper(this);
+        // Find the ListView which will be populated with the pet data
+        ListView petListView = (ListView) findViewById(R.id.listview_pet);
+
+        // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
+        View emptyView = findViewById(R.id.empty_view);
+        petListView.setEmptyView(emptyView);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -86,44 +94,14 @@ public class CatalogActivity extends AppCompatActivity {
 
         Cursor cursor=getContentResolver().query(PetEntry.CONTENT_URI,projection,null,null,null);
 
-        TextView displayView = (TextView) findViewById(R.id.text_view_pet);
+        // Find ListView to populate
+        ListView petListView = findViewById(R.id.listview_pet);
 
-        try {
-            // Create a header in the Text View that looks like this:
-            //
-            // The pets table contains <number of rows in Cursor> pets.
-            // _id - name - breed - gender - weight
-            //
-            // In the while loop below, iterate through the rows of the cursor and display
-            // the information from each column in this order.
-            displayView.setText("The pets table contains " + cursor.getCount() + " pets.\n\n");
-            displayView.append(PetEntry._ID + " - " +
-                    PetEntry.COLUMN_PET_NAME+ " - "  + PetEntry.COLUMN_PET_BREED+ " - "  + PetEntry.COLUMN_PET_GENDER+ " - "  + PetEntry.COLUMN_PET_WEIGHT + "\n");
+        // Setup cursor adapter using cursor from last step
+        PetCursorAdapter petAdapter = new PetCursorAdapter(this, cursor);
 
-            // Figure out the index of each column
-            int idColumnIndex = cursor.getColumnIndex(PetEntry._ID);
-            int nameColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_NAME);
-            int breedColumnIndex= cursor.getColumnIndex(PetEntry.COLUMN_PET_BREED);
-            int genderColumnIndex= cursor.getColumnIndex(PetEntry.COLUMN_PET_GENDER);
-            int weightColumnIndex= cursor.getColumnIndex(PetEntry.COLUMN_PET_WEIGHT);
-
-            // Iterate through all the returned rows in the cursor
-            while (cursor.moveToNext()) {
-                // Use that index to extract the String or Int value of the word
-                // at the current row the cursor is on.
-                int currentID = cursor.getInt(idColumnIndex);
-                String currentName = cursor.getString(nameColumnIndex);
-                String currentbreed= cursor.getString(breedColumnIndex);
-                String currentgender= cursor.getString(genderColumnIndex);
-                int currentweight= cursor.getInt(weightColumnIndex);
-                // Display the values from each column of the current row in the cursor in the TextView
-                displayView.append(("\n" + currentID + " - " + currentName+" - " + currentbreed+" - " + currentgender + " - " + currentweight));
-            }
-        } finally {
-            // Always close the cursor when you're done reading from it. This releases all its
-            // resources and makes it invalid.
-            cursor.close();
-        }
+        // Attach cursor adapter to the ListView
+        petListView.setAdapter(petAdapter);
     }
 
     /**
